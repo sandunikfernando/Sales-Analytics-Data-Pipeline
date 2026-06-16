2884fc4f4dfd4a247ab35c41a3b1f08ae8adc06f
sandunikfernando <sandunifdo28@gmail.com>
2026-06-16 20:57:01 +0530

    updated dag

--- dags/retail_sales_pipeline.py
+++ dags/retail_sales_pipeline.py
@@ -1,34 +1,5 @@
-# from airflow import DAG
-# from airflow.operators.python import PythonOperator
-# from datetime import datetime
-
-# from scripts.create_raw_tables import create_tables
-# from scripts.ingest_api import load_raw_data
-
-# with DAG(
-#     dag_id="retail_sales_pipeline",
-#     start_date=datetime(2025, 1, 1),
-#     schedule="@daily",
-#     catchup=False
-# ) as dag:
-
-#     create_tables_task = PythonOperator(
-#         task_id="create_tables",
-#         python_callable=create_tables
-#     )
-
-#     load_raw_data_task = PythonOperator(
-#         task_id="ingest_raw_data",
-#         python_callable=load_raw_data
-#     )
-
-#     create_tables_task >> load_raw_data_task
-
-
-
-
-
-
+import sys
+sys.path.insert(0, '/opt/airflow')
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
