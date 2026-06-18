d70e229de244e966cf1b83bab361b283fdbb2579
sandunikfernando <sandunifdo28@gmail.com>
2026-06-19 01:11:04 +0530

    Improve model clarity & refactor SQL

--- retail_sales_dbt/models/marts/fct_cart_items.sql
+++ retail_sales_dbt/models/marts/fct_cart_items.sql
@@ -1,38 +1,3 @@
--- select
-
---     p.cart_id,
-
---     p.product_id,
-
---     c.user_id,
-
---     p.quantity,
-
---     p.price,
-
---     p.total as gross_sales,
-
---     p.discount_percentage,
-
---     p.discounted_total as net_sales,
-
---     (p.total - p.discounted_total)
---         as discount_amount,
-
---     c.load_timestamp
-
--- from {{ ref('stg_cart_products') }} p
-
--- join {{ ref('stg_carts') }} c
-
---     on p.cart_id = c.cart_id
-
-
-
-
-
-
-
 with products as (
 
     select *
